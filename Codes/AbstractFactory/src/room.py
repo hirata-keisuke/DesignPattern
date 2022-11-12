0ff87b3e2a0a9c6afb2bcdddd1dc8d5b27d1f8b0
hirata-keisuke <plainpeace39th@gmail.com>
2022-11-12 11:08:29 +0900

    Roomでpropertyを使った

--- Codes/AbstractFactory/src/room.py
+++ Codes/AbstractFactory/src/room.py
@@ -1,4 +1,12 @@
 class Room:
 
     def __init__(self, room_number):
-        self.number = room_number
+        self._number = room_number
+
+    @property
+    def number(self):
+        return self._number
+
+    @number.setter
+    def number(self, rooo_number):
+        self._number = rooo_number
