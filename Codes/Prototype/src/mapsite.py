430b95a4a54c146c44e4c4c9d27bc043c0bf20d2
hirata-keisuke <plainpeace39th@gmail.com>
2022-11-19 09:30:30 +0900

    PrototypeMapsiteにcloneメソッドを持たせた

--- Codes/Prototype/src/mapsite.py
+++ Codes/Prototype/src/mapsite.py
@@ -1,6 +1,12 @@
 from abc import ABC, abstractmethod
 
-class PrototypeDoor(ABC):
+class PrototypeMapsite(ABC):
+
+    @abstractmethod
+    def clone(self):
+        ...
+
+class Door(ABC):
 
     def __init__(self, r1, r2):
         self._room1 = r1
@@ -10,7 +16,10 @@ class PrototypeDoor(ABC):
     def effect(self):
         ...
 
-class SimpleDoor(PrototypeDoor):
+class SimpleDoor(PrototypeMapsite, Door):
+
+    def clone(self):
+        return SimpleDoor(self._room1, self._room2)
 
     def effect(self):
         return "ただのドアです。"
--- Codes/Prototype/test/simple.py
+++ Codes/Prototype/test/simple.py
@@ -3,11 +3,17 @@ import unittest
 
 class SimpleMazeTest(unittest.TestCase):
 
-    def test_door(self):
+    def test_door_effect(self):
         door = SimpleDoor(1, 2)
+        other = door.clone()
         
         self.assertEqual("ただのドアです。", door.effect())
 
+    def test_door_clone(self):
+        door = SimpleDoor(1, 2)
+        other = door.clone()
+        
+        self.assertIsNot(other, door)
 
 if __name__ == "__main__":
     unittest.main()
